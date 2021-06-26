27e88e49b6eb850ce8a1d6b9f564a263cea55882
prernagulati0 <67996489+prernagulati0@users.noreply.github.com>
2021-06-26 10:57:53 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,10 +4,11 @@ const Main = () => {
     
     const getimages = async () => {
         
-        const response = await fetch('dbbdd16700eb2dff99db00e64fb8af42');
+        const response = await fetch('https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&format=json&nojsoncallback=1&api_key=dbbdd16700eb2dff99db00e64fb8af42');
         console.log(response);
         const data =  await response.json() ;
         console.log(data);
+        {* <image src={`https://farm${data.farm}.staticflickr.com/${data.server}/${data.id}_${data.secret}.jpg`} />*}
     }
     useEffect(() => {
         getimages();
